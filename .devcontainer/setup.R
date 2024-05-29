0aecfe88d61d9882d4eebd02c66952cc04b1fdc1
Seth Erickson <sr.erickson@gmail.com>
2024-05-29 22:43:35 +0000

    add .devcontainer

---
+++ .devcontainer/setup.R
@@ -0,0 +1,8 @@
+# register the repositories for The Carpentries and CRAN
+options(repos = c(
+  carpentries = "https://carpentries.r-universe.dev/",
+  CRAN = "https://cran.rstudio.com/"
+))
+
+# Install the template packages to your R library
+install.packages(c("sandpaper", "varnish", "pegboard", "tinkr"))
